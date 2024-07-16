305f2c8082deb19fff6207ea4f8a12c3d15221fe
selva <selvakumarj1807@gmail.com>
2024-07-16 11:06:51 +0530

    auth

---
+++ 04authentication/Profile.php
@@ -0,0 +1,67 @@
+<?php
+session_start();
+
+// Check if user is logged in
+if (!isset($_SESSION['email']) || empty($_SESSION['email'])) {
+    // Redirect to login if not logged in
+    header("Location: SignIn.php");
+    exit;
+}
+
+// Access user information from session variables
+$email = $_SESSION['email'];
+$user_name = $_SESSION['user_name'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Authentication</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarScroll">
+                <ul class="navbar-nav ms-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">SignUp</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="SignIn.php">SignIn</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="LogOut.php">Logout</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Profile.php">Profile</a>
+                    </li>
+
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+    <br>
+
+    <center>
+
+        <h1>Welcome, <?php echo $user_name; ?>!</h1>
+        <p>Your email address is: <?php echo $email; ?></p>
+
+    </center>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
